207a814f1dd5a2a8c938eae31b4b7348800f6c0f
Crab_King <127689590+Crab-K1ng@users.noreply.github.com>
2025-03-31 18:04:33 +0100

    remove zone renderers stuff

--- src/client/java/com/github/puzzle/game/ClientGlobals.java
+++ src/client/java/com/github/puzzle/game/ClientGlobals.java
@@ -18,34 +18,8 @@ import static com.github.puzzle.core.Constants.MOD_ID;
 
 public class ClientGlobals {
 
-    public static List<IZoneRenderer> renderers = new ArrayList<>();
-    public static Map<String,Integer> rendererIndexMap = new HashMap<>();
-    public static int rendererIndex = 0;
-
     public static final Identifier LanguageEnUs = Identifier.of(MOD_ID, "languages/en-US.json");
     public static Language SelectedLanguage;
 
     public static Texture whitePixel = PuzzleGameAssetLoader.LOADER.loadResourceSync(Identifier.of(MOD_ID, "textures/special/white-1x1.png"), Texture.class);
-
-    public static void initRenderers() {
-        Logger LOGGER = LoggerFactory.getLogger("Puzzle | Renderers");
-
-        Reflections ref = new Reflections();
-
-        Set<Class<? extends IZoneRenderer>> classes = ref.getSubTypesOf(IZoneRenderer.class);
-        LOGGER.warn("Getting renderers");
-
-        for (Class<? extends IZoneRenderer> c : classes) {
-            try {
-                LOGGER.warn("\t{}",c.getName());
-                int currrentIndex = renderers.size();
-                renderers.add(c.getDeclaredConstructor().newInstance());
-                rendererIndexMap.put(c.getName(),currrentIndex);
-            } catch (InstantiationException | IllegalAccessException | NoSuchMethodException |
-                     InvocationTargetException e) {
-                LOGGER.warn("Can't use class \"{}\" as renderer",c.getName());
-            }
-
-        }
-    }
 }
